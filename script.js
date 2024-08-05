c9616a476e16b84001a097eec10c14f669ca379b
pratham26110 <130848652+pratham26110@users.noreply.github.com>
2024-08-05 20:48:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+};
